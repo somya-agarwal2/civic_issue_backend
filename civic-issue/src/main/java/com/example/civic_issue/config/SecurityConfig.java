3ed62d98e8ded1ce92f46a4958a36595ff67d725
somya-agarwal2 <somya.23b1531020@abes.ac.in>
2025-09-26 00:43:57 +0530

    files added

--- civic-issue/src/main/java/com/example/civic_issue/config/SecurityConfig.java
+++ civic-issue/src/main/java/com/example/civic_issue/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         // ✅ Apply to all endpoints
-        source.registerCorsConfiguration("/", configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
